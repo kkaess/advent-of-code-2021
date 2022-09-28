7eecf40865f8fa7287334a40e2becb5e21b2dc4b
kkaess <kkaess@github.com>
2022-09-28 14:30:24 -0500

    tweak tests

--- python/tests/test_day1.py
+++ python/tests/test_day1.py
@@ -1,6 +1,6 @@
 from ..day1 import day1
 
 def test_day1():
-    answer_day1,answer_day2 = day1('input/testinput_1_day1.txt')
-    assert answer_day1 == 7
-    assert answer_day2 == 5
+    answer_part1,answer_part2 = day1('input/testinput_1_day1.txt')
+    assert answer_part1 == 7
+    assert answer_part2 == 5
